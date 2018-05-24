b06dca4b8e38c1e820fe76f5660fe74dcfed765e
Babagana Zannah <babagana.tech@gmail.com>
2018-05-24 10:13:44 +0200

    Initiale files

---
+++ README.md
@@ -0,0 +1,33 @@
+# LoremIpsum Reusable Symfony Bundle
+A simple practical meaningful LoremIpsum Symfony Bundle.
+
+
+## Setup
+
+If you've just downloaded the code, congratulations!
+
+To get it working, follow these steps:
+
+**Download Composer dependencies**
+
+Make sure you have [Composer installed](https://getcomposer.org/download/)
+and then run:
+
+```
+composer install
+```
+
+**Start the built-in web server**
+
+You can use Nginx or Apache, but the built-in web server works
+great:
+
+```
+php bin/console server:run
+```
+
+Now check out the site at `http://localhost:8000`
+
+Have fun!
+
+## Thanks!
---
+++ lib/LoremIpsumBundle/src/DependencyInjection/KnpULoremIpsumExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace KnpU\LoremIpsumBundle\DependencyInjection;
+
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class KnpULoremIpsumExtension extends Extension
+{
+
+    /**
+     * Loads a specific configuration.
+     *
+     * @throws \Exception When provided tag is not defined in this extension
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.xml');
+    }
+
+    public function getAlias()
+    {
+        return 'knpu_lorem_ipsum';
+    }
+}
